5fa23f16035cb5a9ce2912bf679d2bf6eb41e796
Sainath-Pichad-DFT <140145138+Sainath-Pichad-DFT@users.noreply.github.com>
2023-10-20 12:23:48 +0530

    Update RestAccountActionService.java

--- RestAccountActionService.java
+++ RestAccountActionService.java
@@ -1,56 +1,59 @@
-package com.aciworldwide.services.rest.accountaction;
+package com.aciworldwide.services.rest.payments.addenda;
 
-import java.util.List;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import com.aciworldwide.uob.api.messages.services.accountaction.AccountActionPortType;
-import com.aciworldwide.uob.api.messages.services.accountaction.AccountActionsFault;
-import com.aciworldwide.uob.api.messages.services.accountaction.UserActionsFault;
-import com.aciworldwide.uob.api.messages.widget.services.GetAccountActionsRqType;
-import com.aciworldwide.uob.api.messages.widget.services.GetAccountActionsRsType;
-import com.aciworldwide.uob.api.messages.widget.services.GetUserActionsRqType;
-import com.aciworldwide.uob.api.messages.widget.services.GetUserActionsRsType;
+import com.aciworldwide.uob.api.messages.payments.ach.services.ACHAddendaGenerateRqType;
+import com.aciworldwide.uob.api.messages.payments.ach.services.ACHAddendaGenerateRsType;
+import com.aciworldwide.uob.api.messages.payments.ach.services.ACHAddendaParseRqType;
+import com.aciworldwide.uob.api.messages.payments.ach.services.ACHAddendaParseRsType;
+import com.aciworldwide.uob.api.messages.payments.ach.services.ACHAddendaValidateRqType;
+import com.aciworldwide.uob.api.messages.payments.ach.services.ACHAddendaValidateRsType;
+import com.aciworldwide.uob.api.messages.service.payments.achaddenda.ACHAddendaFault;
+import com.aciworldwide.uob.api.messages.service.payments.achaddenda.ACHAddendaPortType;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
-/**
- * @author yadgirs REST implementation For Account Actions Service.
- */
-@Api(tags = "Account Action")
-@Path("/accountactions")
-public class RestAccountActionService {
+@Api(tags = { "ACH Addenda" })
+@Path("/addenda/ach")
+public class RestAddendaService {
 
-    private AccountActionPortType service;
+    private ACHAddendaPortType achAddendaService;
 
-    @GET
-    @Path("")
-    @ApiOperation(value = "t")
+    public void setAchAddendaService(ACHAddendaPortType achAddendaService) {
+        this.achAddendaService = achAddendaService;
+    }
+
+    @ApiOperation(value = "3333333$333333333333   Parse the given addenda and return back the addenda details.")
+    @POST
+    @Path("/{sec_code}/parse")
     @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
     @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public GetAccountActionsRsType getAccountActions(@ApiParam(value = "", required = true) @QueryParam("accountIdents") List<String> accountIdents) throws AccountActionsFault {
-        GetAccountActionsRqType request = new GetAccountActionsRqType();
-        request.getAccounts().addAll(accountIdents);
-        return getService().getAccountActions(request);
+    public ACHAddendaParseRsType parseStateWiseAddenda(@ApiParam(value = "", required = true, allowableValues = " ") @QueryParam("sec_code") String paymentType, @ApiParam(value = "Null Path param") ACHAddendaParseRqType request) throws ACHAddendaFault {
+        request.setPaymentType(paymentType);
+        return achAddendaService.parseAddenda(request);
     }
 
-  
-    /**
-     * @return the service
-     */
-    public AccountActionPortType getService() {
-        return service;
+    @ApiOperation(value = "** addenda string using the given addenda details.")
+    @POST
+    @Path("/{sec_code}/generate")
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public ACHAddendaGenerateRsType generateAddenda(@ApiParam(value = "*1 description.", required = true, allowableValues = "PPD,CCD,WEB,CTX,TAX,CHILD") @PathParam("sec_code") String paymentType, @ApiParam(value = "Null Path param") ACHAddendaGenerateRqType request) throws ACHAddendaFault {
+        request.setPaymentType(paymentType);
+        return achAddendaService.generateAddenda(request);
     }
 
-    /**
-     * @param service
-     *            the service to set
-     */
-    public void setService(AccountActionPortType service) {
-        this.service = service;
+    @ApiOperation(value = "&22222222222222222222Validate addenda for a sec_code")
+    @POST
+    @Path("/{sec_code}/validator")
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public ACHAddendaValidateRsType validateAddenda(@ApiParam(value = "Null Path param") ACHAddendaValidateRqType request) throws ACHAddendaFault {
+        return achAddendaService.validateAddenda(request);
     }
 }
